897b8410d9445261213eb112c319a58e93cb8eda
Will Douglas <willdowglas@gmail.com>
2018-05-10 16:37:59 -0300

    Adicionada versão 1 do código, modificado README.md

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # Arduino + Bluetooth
+
 Tutorial de como controlar o robô usando Arduino, Bluetooth, e aplicativo Android.
+
+## Versão do código
+
+Escolha uma das versões abaixo, de acordo com o que o seu robô precisa.
+
+### [Versão 1](/ver01)
+- 2 Pontes H
+- Sem armas
+- Sem SoftwareSerial (rx/rx nos pinos 0 e 1)
+- PWM usado para suavizar as curvas
+
+### [Versão 2]
+- 3 Pontes H
+- Arma usando ponte H
+- Sem SoftwareSerial
+- PWM usado para suavizar as curvas
+
+### [Versão 3]
+- 2 Pontes H
+- Sem armas
+- PWM usado para suavizar as curvas
+
+### [Versão 4]
+- 3 Pontes H
+- Arma usando ponte H
+- SoftwareSerial (rx/tx nos pinos 7 e 8)
+- PWM usado para suavizar as curvas
+
+## Circuito
+
+## Aplicativo Android
+
---
+++ ver01/ver01.ino
@@ -0,0 +1,96 @@
+/*
+ * RFC - Controle do robô
+ * Versão simples - 2 pontes H, sem arma
+ * Autor: Will Douglas
+ *  
+ * Equipamento Necessário:
+ * - Arduino
+ * - Módulo Bluetooth
+ * - 2x Ponte H
+ * - Jumpers, ferramentas
+ */
+
+
+const int ponteH01DIR = 10, ponteH01ESQ = 9,
+          ponteH02DIR = 5,  ponteH02ESQ = 6,
+          pwm = 130; // 0 - 255 => 0% - 100%
+
+bool a = 0, b = 0;
+char c, d;
+
+void zero(){
+  digitalWrite(ponteH01DIR, LOW);
+  digitalWrite(ponteH02DIR, LOW);
+  digitalWrite(ponteH01ESQ, LOW);
+  digitalWrite(ponteH02ESQ, LOW);
+}
+
+void up(){
+  zero();
+  digitalWrite(ponteH01DIR, HIGH);
+  digitalWrite(ponteH02DIR, HIGH);
+}
+
+void down(){
+  zero();
+  digitalWrite(ponteH01ESQ, HIGH);
+  digitalWrite(ponteH02ESQ, HIGH);
+}
+
+void left(){
+  zero();
+  analogWrite(ponteH01ESQ, pwm);
+  analogWrite(ponteH02DIR, pwm);
+}
+
+void righ(){
+  zero();
+  analogWrite(ponteH01DIR, pwm);
+  analogWrite(ponteH02ESQ, pwm);
+}
+
+void setup(){
+  pinMode(ponteH01DIR, OUTPUT);
+  pinMode(ponteH01ESQ, OUTPUT);
+  pinMode(ponteH02DIR, OUTPUT);
+  pinMode(ponteH02ESQ, OUTPUT);
+
+  d = 'S';
+  c = 'S';
+}
+
+void loop(){
+  while (mySerial.available())
+  {
+    d = c;
+    c = mySerial.read();
+    
+    if (c != d)
+    {
+      zero();
+      delay(30);
+    }
+    
+    switch(c)
+    {
+      case 'F':
+        up();
+        break;
+      case 'B':
+        down();
+        break;
+      case 'L':
+        left();
+        break;
+      case 'R':
+        righ();
+        break;
+      case 'S':
+        zero();
+        break;
+      default:
+        zero();
+        break;
+    }
+  }
+}
